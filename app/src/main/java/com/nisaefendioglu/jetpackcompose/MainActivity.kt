219287fbeb3c8d1b6ac300ea160c6b1e86b86dda
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-04-20 21:52:55 +0300

    updated gradle

--- app/src/main/java/com/nisaefendioglu/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/jetpackcompose/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.nisaefendioglu.jetpackcompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text("Hello world!")
+        }
     }
 }
