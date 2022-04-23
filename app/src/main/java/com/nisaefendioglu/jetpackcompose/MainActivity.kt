45ad703515a233ec5c89f822fff75f48d66fb2b0
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-04-23 16:14:59 +0300

    added floating button

--- app/src/main/java/com/nisaefendioglu/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/jetpackcompose/MainActivity.kt
@@ -3,13 +3,41 @@ package com.nisaefendioglu.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            MessageCard("Android Developer")
+            BoxExample()
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun BoxExample() {
+    Box(Modifier.fillMaxSize()) {
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(12.dp),
+            onClick = {}
+        ) {
+            Text("+")
         }
     }
 }
