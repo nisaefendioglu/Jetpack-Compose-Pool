2c42c46ccc35e0fb66f638948ce9669f9d80e732
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-05-01 15:50:37 +0300

    test toast message

--- app/src/main/java/com/nisaefendioglu/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/jetpackcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nisaefendioglu.jetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -11,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,12 +33,19 @@ fun MessageCard(name: String) {
 
 @Composable
 fun BoxExample() {
+    val context = LocalContext.current
     Box(Modifier.fillMaxSize()) {
         FloatingActionButton(
             modifier = Modifier
                 .align(Alignment.BottomEnd)
                 .padding(12.dp),
-            onClick = {}
+            onClick = {
+                Toast.makeText(
+                    context,
+                    "Test",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         ) {
             Text("+")
         }
