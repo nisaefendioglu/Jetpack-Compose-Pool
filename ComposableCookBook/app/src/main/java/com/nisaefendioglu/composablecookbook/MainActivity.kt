6766d190f8b136ab576b63d507cb995eda447dac
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-07-04 20:16:46 +0300

    fix

--- ComposableCookBook/app/src/main/java/com/nisaefendioglu/composablecookbook/MainActivity.kt
+++ ComposableCookBook/app/src/main/java/com/nisaefendioglu/composablecookbook/MainActivity.kt
@@ -3,13 +3,13 @@ package com.nisaefendioglu.composablecookbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.nisaefendioglu.composablecookbook.model.RecipeList
+import com.nisaefendioglu.composablecookbook.model.defaultRecipes
 import com.nisaefendioglu.composablecookbook.ui.theme.ComposableCookBookTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +17,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposableCookBookTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Column(modifier = Modifier.fillMaxSize()) {
+                    TopAppBar(title = {
+                        Text("Cook Book \uD83D\uDC69\uD83C\uDFFB\u200D\uD83C\uDF73")
+                    })
+                    RecipeList(defaultRecipes)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposableCookBookTheme {
-        Greeting("Android")
     }
 }
---
+++ ComposableCookBook/app/src/main/java/com/nisaefendioglu/composablecookbook/model/Recipes.kt
@@ -0,0 +1,12 @@
+package com.nisaefendioglu.composablecookbook.model
+
+import com.nisaefendioglu.composablecookbook.R
+
+val defaultRecipes = listOf(
+    Recipe(R.drawable.noodles, "Ramen", listOf("Noodles", "Eggs", "Mushrooms", "Carrots", "Soy Sauce")),
+    Recipe(R.drawable.croissant, "Croissant", listOf("Butter", "More Butter", "A touch of Butter", "Flour")),
+    Recipe(R.drawable.pizza, "Pizza", listOf("Pizza Dough", "Tomatoes", "Cheese", "Spinach", "Love")),
+    Recipe(R.drawable.produce, "Vegetable Medley", listOf("Vegetables")),
+    Recipe(R.drawable.salad_egg, "Egg Salad", listOf("Eggs", "Mayonnaise", "Paprika", "Mustard")),
+    Recipe(R.drawable.smoothie, "Fruit Smoothie", listOf("Banana", "Kiwi", "Milk", "Cream", "Ice", "Flax seed"))
+)
